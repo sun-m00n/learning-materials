79610800487ae7600c7ebef55722f4358b23b440
sun-m00n <infinityknight024@gmail.com>
2022-12-06 18:59:55 +0530

    code resorted

--- c++ dsa/tst.cpp
+++ c++ dsa/tst.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
-    int rows, columns;
-
-    cout << "Rows : ";
-    cin >> rows;
-    cout << "\nColumns : ";
-    cin >> columns;
-
-    for (int i = rows - 1; i >= 0; i--)
-    {
-        cout << endl;
-        for (int j = columns - 1; j >= 0; j--)
-        {
-            cout << "* ";
-        }
-    }
-
-    return 0;
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
 }
